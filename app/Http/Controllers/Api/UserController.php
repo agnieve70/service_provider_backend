0c96844faa75caaaea8421221a9bd975a488281d
agnieve <ag@castledigital.com.au>
2022-07-19 14:45:41 +0800

    added fixes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
     function register(Request $request){
         $request->validate([
             'name' => 'required',
-            'email' => 'required|unique:user',
+            'email' => 'required|unique:users',
             'password' => 'required',
             'contact' => 'required',
             'firstname' => 'required',
@@ -26,11 +27,27 @@ class UserController extends Controller
         $user->firstname = $request->firstname;
         $user->lastname = $request->lastname;
         $user->role = $request->role;
+        $str_rnd = Str::random(5);
+            $user->remember_token = $str_rnd;
+            $user->password = bcrypt($request->password);
         $user->save();
+        if (!$token = auth()->attempt(["email" => $request->email, "password" => $request->password])) {
+            return response()->json([
+                "errors" => [
+                    "user" => 0
+                ],
+                "message" => "Cannot get token"
+            ], 400);
+        }
+
+        $token = $request->user()->createToken($request->email);
+
         return response()->json([
             "status" => 1,
-            "message" => "Has been Saved",
-            "data"=>$user
+            "message" => "User registered successfully",
+            "user_id" => $user->id,
+            "token" => $token->plainTextToken,
+            "user" => $user
         ], 200);
     }
 }
